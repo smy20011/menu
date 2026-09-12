63e39f05ad980368d2ad91a59b0c76b87a5dfba9
Siyuan Ma <smy20011@gmail.com>
2026-09-12 14:28:58 -0700

    Manual Rewrite of the Generated Menu Script

--- src/menu/generate_menu.py
+++ src/menu/generate_menu.py
@@ -3,10 +3,14 @@
 from dataclasses import dataclass
 from glob import glob
 import json
+from os import PathLike
 from random import shuffle
 import re
 import subprocess
 from typing import Any
+from datetime import datetime, timedelta
+from pathlib import Path
+import argparse
 
 
 @dataclass
@@ -30,8 +34,38 @@ class Recipes:
     def by_tag(self, tag: str) -> list[Recipe]:
         return [r for r in self.recipes if tag in r.metadata.tags]
 
+@dataclass
+class Menu:
+    menu: list[dict[str, Recipe]]
 
-def generate_menu(recipes: Recipes, days=7) -> list[dict[str, Recipe]]:
+    def cooklang_menu(self, starting_date: datetime) -> str:
+        fragments = []
+        for day, menu_day in enumerate(self.menu):
+            date = starting_date + timedelta(day)
+            fragments.append(date.strftime('== %A (%Y-%m-%d) =='))
+            fragments.append("")
+            for category, recipe in self.menu[day].items():
+                fragments.append(f"{category}: @./{recipe.metadata.filename}{{}}")
+                fragments.append("")
+            fragments.append("")
+        return "\n".join(fragments)
+
+    def markdown_menu(self, starting_date: datetime) -> str:
+        fragments = []
+        for day, menu_day in enumerate(self.menu):
+            date = starting_date + timedelta(day)
+            fragments.append(date.strftime('### %A (%Y-%m-%d)'))
+            fragments.append("")
+            for category, recipe in self.menu[day].items():
+                fragments.append(f"- {category}: {recipe.metadata.filename}")
+                fragments.append("")
+            fragments.append("")
+        return "\n".join(fragments)
+
+def cook_cli(*args: str) -> Any:
+    return json.loads(subprocess.run(["cook"] + list(args), text=True, check=True, capture_output=True).stdout)
+
+def generate_menu(recipes: Recipes, days=7) -> Menu:
     categories = ["主食", "肉菜", "素菜"]
     recipes_by_category = {}
     for category in categories:
@@ -49,32 +83,61 @@ def generate_menu(recipes: Recipes, days=7) -> list[dict[str, Recipe]]:
         for category in categories:
             per_day[category] = recipes_by_category[category][d]
         result.append(per_day)
-    return result
+    return Menu(result)
 
 
 def extract_recipes(pathname: str) -> Recipes:
     result = []
     for filename in glob(pathname):
-        res = subprocess.run(
-            ["cook", "recipe", "--format", "json", filename],
-            check=True,
-            capture_output=True,
-            text=True,
-        )
-        data = json.loads(res.stdout)
+        data = cook_cli("recipe", "--format", "json", filename)
         metadata = Metadata(
-            tags = data["metadata"]["map"]["tags"],
-            source = data["metadata"]["map"]["source"],
-            filename = filename,
+            tags=data["metadata"]["map"]["tags"],
+            source=data["metadata"]["map"]["source"],
+            filename=filename,
         )
         recipe = Recipe(
-            name = data["metadata"]["map"]["title"],
-            ingredients = [i["name"] for i in data["ingredients"]],
-            metadata = metadata
+            name=data["metadata"]["map"]["title"],
+            ingredients=[i["name"] for i in data["ingredients"]],
+            metadata=metadata,
         )
         result.append(recipe)
     return Recipes(result)
 
+def write_menu(menu: Menu, date: datetime, dest: Path, override: bool = False):
+    if dest.exists() and not override:
+        raise ValueError(f"{dest} already exists, use -f to override.")
+    dest.write_text(menu.cooklang_menu(date))
+
+def write_report(menu_file: Path, date: datetime, menu: Menu, dest: Path):
+    data = cook_cli("shopping-list", "--format", "json", str(menu_file))
+    result = ["# 菜谱", "## 购物清单"]
+    for category in data:
+        result.append(f"### {category['category']}")
+        result.append("")
+        for item in category:
+            result.append(f"- [ ] {item.name}")
+        result.append("")
+
+    result.append("## 每日菜谱")
+    result.append(menu.markdown_menu(date))
+    dest.write_text("\n".join(result))
+
+def main():
+    parser = argparse.ArgumentParser("Menu Gen: Generate Random Menu")
+    parser.add_argument("-f", '--force', action='store_true', help='Override existing menus')
+    parser.add_argument("--glob", help="Glob pattern for cook files, default *.cook", default="*.cook")
+    parser.add_argument("--days", help="Number of days to generate", default=7, type=int)
+    parser.add_argument('date', help='Generate Menu for following date, should be in format like 2026-01-23')
+    opts = parser.parse_args()
+
+    date = datetime.strptime(opts.date, "%Y-%m-%d")
+
+    recipes = extract_recipes(opts.glob)
+    menu = generate_menu(recipes, opts.days)
+    menu_dest = Path("menus") / date.strftime("%Y-%m-%d.menu")
+    report_dest = Path("menus") / date.strftime("%Y-%m-%d.md")
+    write_menu(menu, date, menu_dest, override=opts.force)
+    write_report(menu_dest, date, menu, report_dest)
 
 if __name__ == "__main__":
     main()
